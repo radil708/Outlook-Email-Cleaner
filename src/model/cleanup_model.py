9c15697a72f4eea776020988ea0d56827f2a9a2a
radil <adil.r@northeastern.edu>
2023-01-23 17:45:50 -0500

    - Added and tested date setters for cleanup_model.py

--- src/model/cleanup_model.py
+++ src/model/cleanup_model.py
@@ -1,5 +1,6 @@
 import win32com.client as client
 from src.model.cleanup_custom_exceptions import *
+from src.model.date_handler import date_handler
 
 """
 This class is meant to have code that connects to outlook, pick
@@ -16,6 +17,8 @@ class cleanup_model():
 		Constructor for the model. Does not require any parameters.
 		It sets up empty attributes for the class.
 		"""
+
+		self.date_utility = date_handler()
 
 		################ variables relating to deleting conditions ################
 		self.target_sender_email = None    # string
@@ -52,3 +55,65 @@ class cleanup_model():
 		else:
 			self.target_subject_keyphrase = subject_key
 			self.verification_add_on[2] = subject_key
+
+	def set_target_start_date(self, date: str):
+		'''
+		Sets the target start date, with time at 00:00:00
+		:param date: {str} representation of datetime, must be in the format mm/dd/yyyy
+		:return: None
+		'''
+		#if empty date input then don't do anything
+		if date.isspace() or date == "":
+			return
+		else:
+			try:
+				date_list_start = self.date_utility.process_str_date(date)
+			except ValueError:
+				raise DateConversionError()
+			self.target_start_date = self.date_utility.convert_list_into_datetime(date_list_start)
+			string_date_start = self.target_start_date.strftime("%m/%d/%Y")
+			self.verification_add_on[1] = string_date_start
+
+	def set_target_end_date(self, date: str):
+		'''
+		Sets the target end date with time at 11:59:59
+		:param date: {str} representation of datetime, must be in the format mm/dd/yyyy
+		:return:
+		'''
+		#if empty date input
+		if date.isspace() or date == "":
+			#check that start date is also empty
+			if self.target_start_date is None:
+				return
+			else:
+				raise MissingStartDate()
+
+		# if input is not empty, but start date is, raise error
+		if self.target_start_date is None:
+			raise MissingStartDate()
+
+		# if start_date is already set, set the end_date
+		try:
+			date_list_end = self.date_utility.process_str_date(date)
+		except ValueError:
+			raise DateConversionError()
+
+		self.target_end_date = self.date_utility.convert_list_into_datetime(date_list_end,endtime=True)
+		date_existing = self.verification_add_on[1]
+		if date_existing == "" or date_existing is None:
+			raise MissingStartDate()
+		else:
+			date_existing += f" to {self.target_end_date.strftime('%m/%d/%Y')}"
+		self.verification_add_on[1] = date_existing
+
+	def clear_deleting_conditions(self):
+		'''
+		Set all deleting conditions (attributes) to None and resetting appropriate attributes
+		:return: None
+		'''
+
+		self.target_sender_email = None
+		self.target_start_date = None
+		self.target_end_date = None
+		self.target_subject_keyphrase = None
+		self.verification_add_on = {}
--- test/test_cleanup_model.py
+++ test/test_cleanup_model.py
@@ -1,5 +1,8 @@
 import unittest
 from src.model.cleanup_model import cleanup_model
+from src.model.date_handler import LOCAL_TIMEZONE
+from datetime import datetime
+from src.model.cleanup_custom_exceptions import *
 
 class test_cleanup_model(unittest.TestCase):
 	@classmethod
@@ -26,6 +29,39 @@ class test_cleanup_model(unittest.TestCase):
 		model.set_target_subject_keyphrase("keyword")
 		self.assertEqual("keyword", model.target_subject_keyphrase)
 		self.assertEqual("keyword", model.verification_add_on[2])
+
+		model.set_target_start_date("2/28/2022")
+		self.assertEqual(datetime(2022,2,28,00,00,00, tzinfo=LOCAL_TIMEZONE), model.target_start_date)
+		self.assertEqual("02/28/2022",model.verification_add_on[1])
+
+		model.set_target_end_date("06/18/2022")
+		self.assertEqual(datetime(2022, 6, 18, 23, 59, 59, tzinfo=LOCAL_TIMEZONE), model.target_end_date)
+		self.assertEqual("02/28/2022 to 06/18/2022", model.verification_add_on[1])
+
+	def test_date_setter_throw_missing_date_error(self):
+		"""
+		Testing that an error is raised if an end date is set while the
+		start date is empty.
+		:return:
+		"""
+		model = self.model
+		model.clear_deleting_conditions()
+		self.assertRaises(MissingStartDate, model.set_target_end_date, "6/18/2022")
+
+	def test_date_setter_throw_date_conv_error(self):
+		"""
+		Testing that incorrect input throws conversion error when date handler
+		throws Value error.
+		:return:
+		"""
+		model = self.model
+		model.clear_deleting_conditions()
+		#invalid start date
+		self.assertRaises(DateConversionError, model.set_target_start_date, "31/15/2021")
+
+		#valid start date but invalid end date
+		model.set_target_start_date("2/28/2022")
+		self.assertRaises(DateConversionError, model.set_target_end_date, "06/82/2022")
 
 def main():
 	unittest.main(verbosity=3)
